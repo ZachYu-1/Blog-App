67b4da4dc041a1ca8dacaacb16edc28780d246e8
ZachYu-1 <zhekunyu9@gmail.com>
2024-03-04 16:49:13 -0500

    new file:   api/.env 	modified:   api/db.js

--- api/db.js
+++ api/db.js
@@ -1,6 +1,13 @@
 // db.js
 import mysql from "mysql2";
 
+load_dotenv();
+
+db_host = os.getenv('DB_HOST');
+db_user = os.getenv('DB_USER');
+db_password = os.getenv('DB_PASSWORD');
+db_database = os.getenv('DB_DATABASE');
+
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
